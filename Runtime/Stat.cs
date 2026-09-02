e61ba0fd5521d1a52f77464bad79011daa852317
Janek <jasiogamer@gmail.com>
2026-09-02 11:18:57 +0200

    save variable to file

---
+++ Runtime/Stat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Runtime
+{
+    [Serializable]
+    public class Stat<T>
+    {
+        
+    }
+}
---
+++ Runtime/StatTest.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Runtime
+{
+    public class StatTest : MonoBehaviour
+    {
+        [SerializeField] private Stat<int> _intStat;
+    }
+}
