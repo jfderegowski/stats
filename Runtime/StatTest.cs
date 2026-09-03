445e46e39754e475cbc18711c2aa3ecf08c35eff
Janek <jasiogamer@gmail.com>
2026-09-03 15:52:02 +0200

    serialziacja

--- Runtime/StatTest.cs
+++ Runtime/StatTest.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Runtime
 {
+    [Serializable]
+    public class TestStruct
+    {
+        [SerializeField] private int _int;
+    }
+    
     public class StatTest : MonoBehaviour
     {
+        [SerializeField] private int _exampleNumber;
         [SerializeField] private Stat<int> _intStat;
+        [SerializeField] private TestStruct _testStruct;
     }
 }
